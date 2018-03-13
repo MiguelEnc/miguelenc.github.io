e873c47c58fba89bbd5183d471bc91ab2c7ed2a0
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2018-03-13 01:18:26 +0000

    Add top and side nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-var myButton = document.getElementById("clickButton");
-var myText = document.getElementById("helloText");
-
-myButton.addEventListener('click', doSomething, false)
-
-function doSomething() {
-	myText.textContent = "hello, world!";
-}
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
